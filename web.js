fa5c0bb5b82b2a968ca6252d61ed76d9b31acc21
Ubuntu <ubuntu@ip-172-31-28-13.us-west-2.compute.internal>
2013-07-06 09:11:53 +0000

    Updated web.js with a bug fix

--- web.js
+++ web.js
@@ -4,8 +4,8 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var fs = require("fs");
-  var buffer = new Buffer();	
-  	response.send(buffer.toString('utf-8', 0, fs.readFileSync('index.html')));
+  var buffer = new Buffer(fs.readFileSync('index.html'),'utf-8');	
+  	response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
